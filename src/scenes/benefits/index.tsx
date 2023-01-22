0acd183249754b7933ceefc23d1867f297fc4d7d
Yurii Rybachok <drummer2009@ukr.net>
2023-01-22 17:03:29 +0200

    add cards with data to benefits page

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,7 +1,31 @@
 import HText from "@/shared/HText";
-import { SelectedPage } from "@/shared/types"
+import { BenefitType, SelectedPage } from "@/shared/types"
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid";
 import { motion } from 'framer-motion';
+import Benefit from "./Benefit";
+
+const benefits: Array<BenefitType> = [
+    {
+        icon: <HomeModernIcon className="h-6 w-6" />,
+        title: "State of the Art Facilities",
+        description: 
+        "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et."
+    },
+    {
+        icon: <UserGroupIcon className="h-6 w-6" />,
+        title: "100's of Diverse Classes",
+        description:
+          "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+      },
+      {
+        icon: <AcademicCapIcon className="h-6 w-6" />,
+        title: "Expert and Pro Trainers",
+        description:
+          "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+      },
+]
+
+
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -23,6 +47,17 @@ const Benefits = ({setSelectedPage}: Props) => {
                 </p>
             </div>
             {/* BENEFITS */}
+            <div className="md:flex items-center justify-between gap-8 mt-5 ">
+                {benefits.map((benefit: BenefitType) => (
+                    <Benefit
+                        key={benefit.title}
+                        icon={benefit.icon}
+                        title={benefit.title}
+                        description={benefit.description}
+                        setSelectedPage={setSelectedPage}
+                    />
+                ))}
+            </div>
         </motion.div>
     </section>
   )
