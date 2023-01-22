c449dcf75b38b2b2a905ed96d283f2e59df4563b
Yurii Rybachok <drummer2009@ukr.net>
2023-01-22 15:11:19 +0200

    add classes for text, images and sponsor in home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -19,25 +19,25 @@ const Home = ({setSelectedPage}: Props) => {
         id='home'
         className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'
     >
-        {/* IMAGE AND MEIN HEADER */}
-        <div>
+        {/* IMAGE AND MAIN HEADER */}
+        <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
             {/* MAIN HEADER */}
-            <div>
+            <div className='z-10 mt-32 md:basis-3/5'>
                 {/* HEADINGS */}
-                <div>
-                    <div>
-                        <div>
+                <div className='md:-mt-20'>
+                    <div className='relative'>
+                        <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext'>
                             <img alt='home-page-text' src={HomePageText} />
                         </div>
                     </div>
-                    <p>
+                    <p className='mt-8 text-sm'>
                         Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
                         Studios to get the Body Shapes That you Dream of.. Get Your Dream
                         Body Now.
                     </p>
                 </div>
                 {/* ACTIONS */}
-                <div>
+                <div className='mt-8 flex items-center gap-8'>
                     <ActionButton setSelectedPage={setSelectedPage}>
                         Join Now
                     </ActionButton>
@@ -52,11 +52,22 @@ const Home = ({setSelectedPage}: Props) => {
             </div>
 
             {/* IMAGE */}
-            <div>
+            <div className='flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end'>
                 <img alt='home-pageGraphic' src={HomePageGraphic} />
             </div>
         </div>
-        
+        {/* SPONSORS */}
+        {isAboveMediumScreens && (
+            <div className='h-[150px] w-full bg-primary-100 py-10'>
+                <div className='mx-auto w-5/6 '>
+                    <div className='flex w-3/5 items-center justify-between gap-8'>
+                        <img alt='redbull-sponsor' src={SponsorRedBull} />
+                        <img alt='forbes-sponsor' src={SponsorForbes} />
+                        <img alt='fortune-sponsor' src={SponsorFortune} />
+                    </div>
+                </div>
+            </div>
+        )}
     </section>
   )
 }
