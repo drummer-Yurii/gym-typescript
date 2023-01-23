183ac6f88764b32814054ca2cd4c7751bca8ff19
Yurii Rybachok <drummer2009@ukr.net>
2023-01-23 10:38:42 +0200

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from '@/scenes/home';
 import OurClasses from '@/scenes/ourClasses';
 import Benefits from '@/scenes/benefits';
 import ContactUs from '@/scenes/contactUs';
-import Footer from './scenes/footer';
+import Footer from '@/scenes/footer';
 import { useEffect, useState } from 'react';
 import { SelectedPage } from '@/shared/types';
 
@@ -35,7 +35,7 @@ function App() {
       <ContactUs setSelectedPage={setSelectedPage} />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
